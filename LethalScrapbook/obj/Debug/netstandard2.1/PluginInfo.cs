83ac7e8a23bea80b02b04d65de08daae8b96da92
Ryan Kleeberger <rylklee@gmail.com>
2024-03-03 17:22:37 -0600

    Add new files and update existing files

---
+++ LethalScrapbook/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LethalScrapbook
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "LethalScrapbook";
+        public const string PLUGIN_NAME = "LethalScrapbook";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
